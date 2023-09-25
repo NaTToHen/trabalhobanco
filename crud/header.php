2f490f4d439a3f2752e42c8c4f5511517dcb6632
Henrique Bonatto <nattohen@gmail.com>
2023-09-25 10:41:32 -0300

    02

---
+++ crud/header.php
@@ -0,0 +1,9 @@
+<h1>Crud banco de dados</h1>
+   <header>
+      <ul>
+         <li class="link"><a href="adicionar.php?id=">Adicionar</a></li>
+         <li class="link"><a href="editar.php?id">Editar</a> <input type="text" class="id"></li>
+         <li class="link"><a href="excluir.php?id">Excluir</a> <input type="text" class="id"></li>
+         <li class="link"><a href="ler.php?id">Ler</a></li>
+      </ul>
+   </header>
